075129b897017efccf23267d3598b0cf1f091b28
김호연 <hy940313@gmail.com>
2020-09-16 15:39:07 +0900

    group_dictionary len을 sum으로 수정

--- djangobackend/demacia/group_dictionary.py
+++ djangobackend/demacia/group_dictionary.py
@@ -4,7 +4,8 @@ champ_dictionary = dict()
 
 
 for i in range(877):
-    if i not in champ_dictionary and len(champion_group_2_array[i]) > 0 :
+    if i not in champ_dictionary and sum(champion_group_2_array[i]) > 0 :
         champ_dictionary[i] = list(numpy.argsort(champion_group_2_array[i])[::-1][:4])
+
 
 print(champ_dictionary)
