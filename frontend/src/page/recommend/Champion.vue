7ca5793acdd98b41ef483d23f1174877fb18f506
송다희 <jilhdd@naver.com>
2020-09-21 03:19:57 +0900

    FE-wireframe 추천 페이지 만들기

---
+++ frontend/src/page/recommend/Champion.vue
@@ -0,0 +1,81 @@
+<template>
+<b-container class="bv-example-row bv-example-row-flex-cols">
+  <b-row align-v="start">
+    <b-col><h1>챔피언 추천</h1></b-col>
+  </b-row>
+<b-row align-v="stretch">
+    <b-col cols="4">
+        <h3>1위</h3>
+        <img alt="챔피언 사진"/>   
+    </b-col>
+    <b-col cols="8">
+        <b-row align-v="start">
+            <b-col>xxx님이 가장 잘하는 챔피언은 어쩌구저쩌구</b-col>
+        </b-row>
+        <b-row align-v="start">
+            <b-col>비슷한 성향의 다른 챔피언들</b-col>
+        </b-row>
+    </b-col>
+  </b-row>
+
+  <b-row align-v="stretch">
+    <b-col cols="4">
+        <h3>2위</h3>
+        <img alt="챔피언 사진"/>   
+    </b-col>
+    <b-col cols="8">
+        <b-row align-v="start">
+            <b-col>xxx님이 가장 잘하는 챔피언은 어쩌구저쩌구</b-col>
+        </b-row>
+        <b-row align-v="start">
+            <b-col>비슷한 성향의 다른 챔피언들</b-col>
+        </b-row>
+    </b-col>
+  </b-row>
+
+    <b-row align-v="stretch">
+    <b-col cols="4">
+        <h3>3위</h3>
+        <img alt="챔피언 사진"/>   
+    </b-col>
+    <b-col cols="8">
+        <b-row align-v="start">
+            <b-col>xxx님이 가장 잘하는 챔피언은 어쩌구저쩌구</b-col>
+        </b-row>
+        <b-row align-v="start">
+            <b-col>비슷한 성향의 다른 챔피언들</b-col>
+        </b-row>
+    </b-col>
+  </b-row>
+
+</b-container>
+
+
+</template>
+
+<script>
+import "../../assets/css/champion.css";
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+
+  created() {
+    
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/page/recommend/Combination.vue
@@ -0,0 +1,84 @@
+<template>
+<b-container class="bv-example-row bv-example-row-flex-cols">
+  <b-row align-v="start">
+    <b-col><h1>조합 추천</h1></b-col>
+  </b-row>
+<b-row align-v="stretch">
+    <b-col>
+        <h3>2명파티</h3>  
+    </b-col>
+    <b-col>
+        <h5>1위</h5>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <h5>2위</h5>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <h5>3위</h5>
+        <img alt="사진"/>
+    </b-col>
+  </b-row>
+
+  <b-row align-v="stretch">
+    <b-col>
+        <h3>3명파티</h3>  
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+  </b-row>
+
+<b-row align-v="stretch">
+    <b-col>
+        <h3>5명파티</h3>  
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+    <b-col>
+        <img alt="사진"/>
+    </b-col>
+  </b-row>
+
+</b-container>
+
+
+</template>
+
+<script>
+import "../../assets/css/combination.css";
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+
+  created() {
+    
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<style>
+
+</style>
