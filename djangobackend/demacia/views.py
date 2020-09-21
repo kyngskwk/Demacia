ccc0b27906ed3e88b3c119842b94fa14e68836b7
김호연 <hy940313@gmail.com>
2020-09-21 14:34:58 +0900

    swagger 만들기 성공

--- djangobackend/demacia/views.py
+++ djangobackend/demacia/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import ChampionSerializer
+from .models import Champion
 # Create your views here.
 
 def test(request):
     return render(request, 'demacia/test.html')
+
+@api_view(['GET'])
+def champion_list(request):
+    champions = Champion.objects.all()
+    serializers = ChampionSerializer(champions, many=True)
+    return Response(serializers.data)
+
