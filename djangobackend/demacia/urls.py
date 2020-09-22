c9e5c8997bd17be45d0c7d68620944d6b1f99743
김호연 <hy940313@gmail.com>
2020-09-22 14:12:24 +0900

    url 세팅중

--- djangobackend/demacia/urls.py
+++ djangobackend/demacia/urls.py
@@ -10,11 +10,11 @@ urlpatterns = [
     path('champions/', views.champion_list, name="champion list"),
     path('videoposts/', views.videopost_list, name="video list"),
     path('matches/', views.match_list, name="match list"),
-    path('videopostlikes/', views.videopostlike_list, name="videopostlike list"),
-    path('videopostlikes/<int:videopost_pk>', views.videopostlike_delete, name="videopostlike delete"),
-    path('videopostlikes/<int:videopost_pk>', views.videopostlike_create, name="videopostlike create"),
-    path('replylikes/', views.replylike_list, name="replylike list"),
-    path('replylikes/<int:reply_pk>', views.replylike_delete, name="replylike delete"),
-    path('replylikes/<int:reply_pk>', views.replylike_create, name="replylike create"),
+    path('videoposts/<int:videopost_pk>/likes/list', views.videopostlike_list, name="videopostlike list"),
+    path('videoposts/<int:videopost_pk>/likes/<int:userno_pk>', views.videopostlike_delete, name="videopostlike delete"),
+    path('videoposts/<int:videopost_pk>/likes/<int:userno_pk>', views.videopostlike_create, name="videopostlike create"),
+    path('videoposts/<int:videopost_pk>/reply/<int:reply_pk>', views.replylike_list, name="replylike list"),
+    path('videoposts/<int:videopost_pk>/reply/<int:reply_pk>', views.replylike_delete, name="replylike delete"),
+    path('videoposts/<int:videopost_pk>/reply/<int:reply_pk>', views.replylike_create, name="replylike create"),
 
 ]
