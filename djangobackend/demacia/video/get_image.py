ed425d11e5466e1788fb1292abd3318c6d585ceb
조완석 <wansuk771@gmail.com>
2020-09-25 12:58:49 +0900

    BE-영상 OCR 단일화 작업

--- djangobackend/demacia/video/get_image.py
+++ djangobackend/demacia/video/get_image.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+
 import cv2
+import change_text 
  
 # 영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
-vidcap = cv2.VideoCapture('./10-19_KR-4664501922_06.mp4')
+vidcap = cv2.VideoCapture('./람머스.mp4')
  
 count = 0
 
 length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+print(length)
 while(vidcap.isOpened()):
     ret, image = vidcap.read()
     if int(vidcap.get(1)) == 50:
@@ -21,3 +24,5 @@ while(vidcap.isOpened()):
         count += 1
     elif int(vidcap.get(1)) == (length-10):
         break
+
+change_text.module()
