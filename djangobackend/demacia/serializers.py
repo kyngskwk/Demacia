b67e71f4eba105f04a8f261e6cf2ede52aac00a2
김호연 <hy940313@gmail.com>
2020-10-05 21:25:50 +0900

    막무가내 response

--- djangobackend/demacia/serializers.py
+++ djangobackend/demacia/serializers.py
@@ -1,4 +1,4 @@
-from .models import Champion, Videopost , Match, Replylikes , Videopostlikes, User
+from .models import Champion, Videopost, Match, Replylikes , Videopostlikes, User
 from rest_framework import serializers, viewsets
 # Seriallizer는 API를 통한 요청에 대한 응답의 형태를을 결정해주는 클래스이다. 
 # ViewSet은 요청을 처리하여 응답을 해주는 클래스이다. 
@@ -18,14 +18,14 @@ class ChampionViewSet(viewsets.ModelViewSet):
     queryset = Champion.objects.all()
     serializer_class = ChampionSerializer
 
-class VideopostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Videopost
-        fields = ['videopostno']   
+# class VideopostSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Videopost
+#         fields = '__all__'
 
-class VideopostViewSet(viewsets.ModelViewSet):
-    queryset = Videopost.objects.all()
-    serializer_class =  VideopostSerializer
+# class VideopostViewSet(viewsets.ModelViewSet):
+#     queryset = Videopost.objects.all()
+#     serializer_class =  VideopostSerializer
  
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
@@ -36,23 +36,23 @@ class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all()
     serializer_class =  MatchSerializer
 
-class ReplylikesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Replylikes
-        fields = '__all__'   
+# class ReplylikesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Replylikes
+#         fields = '__all__'   
 
-class ReplylikesViewSet(viewsets.ModelViewSet):
-    queryset = Replylikes.objects.all()
-    serializer_class =  ReplylikesSerializer
+# class ReplylikesViewSet(viewsets.ModelViewSet):
+#     queryset = Replylikes.objects.all()
+#     serializer_class =  ReplylikesSerializer
 
-class VideopostlikesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Videopostlikes
-        fields = '__all__'   
+# class VideopostlikesSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Videopostlikes
+#         fields = '__all__'   
 
-class VideopostlikesViewSet(viewsets.ModelViewSet):
-    queryset = Videopostlikes.objects.all()
-    serializer_class =  VideopostlikesSerializer
+# class VideopostlikesViewSet(viewsets.ModelViewSet):
+#     queryset = Videopostlikes.objects.all()
+#     serializer_class =  VideopostlikesSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -64,6 +64,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['userno', 'mbti']
+
 class VideoUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
