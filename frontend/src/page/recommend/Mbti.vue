9cd2f7e86aa0235e91647ba016e6c09e05861199
조완석 <wansuk771@gmail.com>
2020-10-05 17:24:31 +0900

    FE-MBTI디자인

--- frontend/src/page/recommend/Mbti.vue
+++ frontend/src/page/recommend/Mbti.vue
@@ -1,16 +1,18 @@
 <template>
   <b-container class="bv-example-row bv-example-row-flex-cols">
-    <b-card class="m-2">
-      <b-row align-v="start">
-        <b-col><h1>MBTI 추천</h1></b-col>
-      </b-row>
-
+    <b-card class="m-2 chambox" style="color:white;">
       <b-row align-v="start">
         <b-col>
-          <h2>{{ user.userNickname }}님의 MBTI는 '{{ user.mbti }}' 입니다.</h2>
+          <h2>
+            <strong style="color:yellow;">{{ user.userNickname }}</strong
+            >님의 MBTI는
+            <strong style="color:red;">'{{ user.mbti }}'</strong> 입니다.
+          </h2>
         </b-col>
       </b-row>
-      <h4 class="text-center"><br />[여기에 {{ user.mbti }}에 대한 설명 입력]</h4>
+      <h4 class="text-center">
+        <br />[여기에 {{ user.mbti }}에 대한 설명 입력]
+      </h4>
       <b-container class="p-4">
         <b-jumbotron class="m-4 p-4">
           <b-row align-v="start">
@@ -22,9 +24,11 @@
               md="3"
               v-for="(item, $index) in chams.mbti_chams"
               :key="$index"
-              class="text-left"
+              class="text-left mt-3"
+              style="font-size:20px;"
             >
               <b-img
+                class="chamimg"
                 thumbnail
                 width="100%"
                 :src="
@@ -41,16 +45,23 @@
         <b-jumbotron class="m-4 p-4">
           <b-row align-v="start">
             <b-col cols="12">
-              <h2>1. 극강의 조합 : {{ chams.good_chams[0].mbti }}</h2>
+              <h2>
+                최상의 조합 :
+                <strong style="color:#51b9d6">{{
+                  chams.good_chams[0].mbti
+                }}</strong>
+              </h2>
             </b-col>
             <b-col
               cols="12"
               md="3"
               v-for="(item, $index) in chams.good_chams"
               :key="$index"
-              class="text-left"
+              class="text-left mt-3"
+              style="font-size:20px;"
             >
               <b-img
+                class="chamimg"
                 thumbnail
                 width="100%"
                 :src="
@@ -67,16 +78,23 @@
         <b-jumbotron class="m-4 p-4">
           <b-row align-v="start">
             <b-col cols="12">
-              <h2>2. 최악의 조합 : {{ chams.bad_chams[0].mbti }}</h2>
+              <h2>
+                최악의 조합 :
+                <strong style="color:#51b9d6">{{
+                  chams.bad_chams[0].mbti
+                }}</strong>
+              </h2>
             </b-col>
             <b-col
               cols="12"
               md="3"
               v-for="(item, $index) in chams.bad_chams"
               :key="$index"
-              class="text-left"
+              class="text-left mt-3"
+              style="font-size:20px;"
             >
               <b-img
+                class="chamimg"
                 thumbnail
                 width="100%"
                 :src="
@@ -128,4 +146,23 @@ export default {
 </script>
 
 <style>
+.chambox {
+  border-style: ridge;
+  border: #e3d19e 1px ridge;
+  opacity: 0.8;
+  background: linear-gradient(
+    180deg,
+    rgba(6, 17, 27, 1) 0%,
+    rgba(28, 83, 73, 1) 100%
+  );
+}
+.chamimg {
+  border-style: ridge;
+  border: #fcd000 5px ridge;
+  border-radius: 50%;
+}
+.jumbotron {
+  background-color: #111822;
+  border: 3px ridge rgb(252, 208, 0);
+}
 </style>
