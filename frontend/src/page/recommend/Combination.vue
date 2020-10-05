225ca39a535d5086efda5803c5b7c6b675f5af77
kyngskwk <kyngskwk@gmail.com>
2020-10-05 13:07:48 +0900

    FE-sk change combination recommend page

--- frontend/src/page/recommend/Combination.vue
+++ frontend/src/page/recommend/Combination.vue
@@ -205,183 +205,184 @@
       </b-row>
     </div>
 
-    <b-card>
-
-      <b-row align-v="stretch" class="section">
-        <b-col cols="2">
-            <h3>5명파티</h3>  
-        </b-col>
-
+    <!-- 5명 파티 -->
+    <div class="box px-3 chambox mb-5 pb-0">
+      <div class="mb-5">
+        <div class="chamtitle pl-4"><p>5 PLAYER</p></div>
+      </div>
+      <b-row align-v="stretch" class="section mb-0">
         <b-col>
-            <b-row align-v="center">
+          <div class="mb-5">
+            <img src="../../assets/img/1badge.png" alt="" class="rankimg">
+          </div>
+          <b-row align-v="center" class="mb-3">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="first_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{first_cham.korname}}</h6>
-            <h6>({{first_cham.chamname}})</h6>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="first_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{first_cham.korname}} | {{first_cham.chamname}}</b-badge>
             </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[0].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[0].korname}}</h6>
-            <h6>({{comb_info[0].chamname}})</h6>
-          </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[0].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[0].korname}} | {{comb_info[0].chamname}}</b-badge>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[1].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[1].korname}}</h6>
-            <h6>({{comb_info[1].chamname}})</h6>
-          </b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[1].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <div class="mb-3">
+                <b-badge pill variant="warning" class="pickname">{{comb_info[1].korname}} | {{comb_info[1].chamname}}</b-badge>
+              </div>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[2].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[2].korname}}</h6>
-            <h6>({{comb_info[2].chamname}})</h6>
-          </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[3].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[3].korname}}</h6>
-            <h6>({{comb_info[3].chamname}})</h6>
-          </b-col>
-          </b-row>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[2].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[2].korname}} | {{comb_info[2].chamname}}</b-badge>
+            </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[3].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[3].korname}} | {{comb_info[3].chamname}}</b-badge>
+            </b-col>
+            </b-row>
         </b-col>
 
         <b-col class="article">
-            <b-row align-v="center">
+          <div class="mb-5">
+            <img src="../../assets/img/2badge.png" alt="" class="rankimg">
+          </div>
+          
+          <b-row align-v="center" class="mb-3">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="second_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{second_cham.korname}}</h6>
-            <h6>({{second_cham.chamname}})</h6>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="second_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{second_cham.korname}} | {{second_cham.chamname}}</b-badge>
             </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[4].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[4].korname}}</h6>
-            <h6>({{comb_info[4].chamname}})</h6>
-          </b-col>
+
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[4].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[4].korname}} | {{comb_info[4].chamname}}</b-badge>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[5].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[5].korname}}</h6>
-            <h6>({{comb_info[5].chamname}})</h6>
-          </b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[5].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <div class="mb-3">
+                <b-badge pill variant="warning" class="pickname">{{comb_info[5].korname}} | {{comb_info[5].chamname}}</b-badge>
+              </div>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[6].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[6].korname}}</h6>
-            <h6>({{comb_info[6].chamname}})</h6>
-          </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[7].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[7].korname}}</h6>
-            <h6>({{comb_info[7].chamname}})</h6>
-          </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[6].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[6].korname}} | {{comb_info[6].chamname}}</b-badge>
+            </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[7].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[7].korname}} | {{comb_info[7].chamname}}</b-badge>
+            </b-col>
           </b-row>
         </b-col>
 
         <b-col>
-            <b-row align-v="center">
+          <div class="mb-5">
+            <img src="../../assets/img/3badge.png" alt="" class="rankimg">
+          </div>
+
+          <b-row align-v="center" class="mb-3">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="third_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{third_cham.korname}}</h6>
-            <h6>({{third_cham.chamname}})</h6>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="third_cham.imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{third_cham.korname}} | {{third_cham.chamname}}</b-badge>
             </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[8].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[8].korname}}</h6>
-            <h6>({{comb_info[8].chamname}})</h6>
-          </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[8].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[8].korname}} | {{comb_info[8].chamname}}</b-badge>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
             <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[9].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[9].korname}}</h6>
-            <h6>({{comb_info[9].chamname}})</h6>
-          </b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[9].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <div class="mb-3">
+                <b-badge pill variant="warning" class="pickname">{{comb_info[9].korname}} | {{comb_info[9].chamname}}</b-badge>
+              </div>
+            </b-col>
           </b-row>
 
           <b-row align-v="center">
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[10].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[10].korname}}</h6>
-            <h6>({{comb_info[10].chamname}})</h6>
-          </b-col>
-          <b-col>
-            <b-img
-                    style="border-style:ridge; border: #fcd000 3px ridge;"
-                    width="100%"
-                    :src="comb_info[11].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
-            />
-            <h6>{{comb_info[11].korname}}</h6>
-            <h6>({{comb_info[11].chamname}})</h6>
-          </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[10].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[10].korname}} | {{comb_info[10].chamname}}</b-badge>
+            </b-col>
+            <b-col>
+              <b-img
+                      style="border-style:ridge; border: #fcd000 3px ridge;"
+                      width="100%"
+                      :src="comb_info[11].imgsrc.replace('@/assets/img', '').replace('_1.', '_0.')"
+              />
+              <b-badge pill variant="warning" class="pickname">{{comb_info[11].korname}} | {{comb_info[11].chamname}}</b-badge>
+            </b-col>
           </b-row>
         </b-col>
-
       </b-row>
-    </b-card>
+    </div>
   </b-container>
 
 
