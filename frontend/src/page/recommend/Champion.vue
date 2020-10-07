b39c187cda26bf30dd11b8a7224579db7a230f83
조완석 <wansuk771@gmail.com>
2020-10-07 19:43:40 +0900

    FE-champion.vue

--- frontend/src/page/recommend/Champion.vue
+++ frontend/src/page/recommend/Champion.vue
@@ -1,5 +1,8 @@
 <template>
-  <b-container class="bv-example-row bv-example-row-flex-cols">
+  <b-container
+    class="bv-example-row bv-example-row-flex-cols"
+    style="font-family: Tmon"
+  >
     <div>
       <img src="../../assets/img/picktitle.png" alt="" class="mt-5 mb-3" />
     </div>
@@ -7,8 +10,10 @@
 
     <!-- 1위 챔피언 -->
     <div class="box px-3 pb-3 chambox mb-5">
-      <div>
-        <div class="chamtitle pl-4"><p>FIRST PICK</p></div>
+      <div class="mb-2">
+        <div class="chamtitle pl-4 px-auto">
+          <h5 style="color: black">나만의 2인 최고 조합 추천</h5>
+        </div>
       </div>
       <b-row align-v="stretch" class="mb-0">
         <b-col cols="4" class="d-flex justify-content-center">
@@ -23,17 +28,17 @@
                   .replace('_1.jpg', '.png')
               "
             />
-            <b-badge pill variant="warning" class="pickname"
+            <b-badge pill variant="warning" class="pickname size1"
               >{{ cham_no1.chamname }} | {{ cham_no1.korname }}</b-badge
             >
-            <b-progress class="mt-3">
+            <b-progress class="mt-3" style="height:40px;">
               <b-progress-bar
                 :value="cham_no1_rate"
                 variant="info"
                 striped
                 :animated="animate"
               >
-                <span
+                <span style="font-size:20px;"
                   >승률: <strong>{{ cham_no1_rate }}%</strong></span
                 >
               </b-progress-bar>
@@ -44,7 +49,7 @@
           <b-row align-v="center" class="text-white">
             <b-col
               ><h3>
-                <span class="text-warning">{{ user.userNickname }}</span
+                <span class="text-red">{{ user.userNickname }}</span
                 >님이 가장 잘하는 챔피언은
                 <span class="text-warning">{{ cham_no1.korname }}</span>
               </h3></b-col
@@ -59,12 +64,14 @@
             >
           </b-row>
           <div class="simchams mt-5">
-            <div class="simtitle mb-5"><p>비슷한 성향을 가진 챔피언들</p></div>
+            <div class="simtitle mb-5">
+              <h5 style="color: black">비슷한 성향을 가진 챔피언들</h5>
+            </div>
             <b-row align-v="center">
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[0].imgsrc
                       .replace('@/assets/img', '')
@@ -75,7 +82,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[1].imgsrc
                       .replace('@/assets/img', '')
@@ -86,7 +93,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[2].imgsrc
                       .replace('@/assets/img', '')
@@ -97,19 +104,19 @@
             </b-row>
             <b-row align-v="center">
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[0].chamname }} |
                   {{ sim_info[0].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[1].chamname }} |
                   {{ sim_info[1].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[2].chamname }} |
                   {{ sim_info[2].korname }}</b-badge
                 >
@@ -123,7 +130,9 @@
     <!-- 2위 챔피언 -->
     <div class="box px-3 pb-3 chambox mb-5">
       <div>
-        <div class="chamtitle pl-3"><p>SECOND PICK</p></div>
+        <div class="chamtitle pl-4 px-auto">
+          <h5 style="color: black">나만의 2인 최고 조합 추천</h5>
+        </div>
       </div>
       <b-row align-v="stretch" class="mb-0">
         <b-col cols="4" class="d-flex justify-content-center">
@@ -138,17 +147,17 @@
                   .replace('_1.jpg', '.png')
               "
             />
-            <b-badge pill variant="warning" class="pickname"
+            <b-badge pill variant="warning" class="pickname size1"
               >{{ cham_no2.chamname }} | {{ cham_no2.korname }}</b-badge
             >
-            <b-progress class="mt-3">
+            <b-progress class="mt-3" style="height:40px;">
               <b-progress-bar
                 :value="cham_no2_rate"
                 variant="info"
                 striped
                 :animated="animate"
               >
-                <span
+                <span style="font-size:20px;"
                   >승률: <strong>{{ cham_no2_rate }}%</strong></span
                 >
               </b-progress-bar>
@@ -159,7 +168,7 @@
           <b-row align-v="center" class="text-white">
             <b-col
               ><h3>
-                <span class="text-warning">{{ user.userNickname }}</span
+                <span class="text-red">{{ user.userNickname }}</span
                 >님이 가장 잘하는 챔피언은
                 <span class="text-warning">{{ cham_no2.korname }}</span>
               </h3></b-col
@@ -174,12 +183,14 @@
             >
           </b-row>
           <div class="simchams mt-5">
-            <div class="simtitle mb-5"><p>비슷한 성향을 가진 챔피언들</p></div>
+            <div class="simtitle mb-5">
+              <h5 style="color: black">비슷한 성향을 가진 챔피언들</h5>
+            </div>
             <b-row align-v="center">
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[3].imgsrc
                       .replace('@/assets/img', '')
@@ -190,7 +201,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[4].imgsrc
                       .replace('@/assets/img', '')
@@ -201,7 +212,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[5].imgsrc
                       .replace('@/assets/img', '')
@@ -212,19 +223,19 @@
             </b-row>
             <b-row align-v="center">
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[3].chamname }} |
                   {{ sim_info[3].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[4].chamname }} |
                   {{ sim_info[4].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[5].chamname }} |
                   {{ sim_info[5].korname }}</b-badge
                 >
@@ -238,7 +249,9 @@
     <!-- 3위 챔피언 -->
     <div class="box px-3 pb-3 chambox mb-5">
       <div>
-        <div class="chamtitle pl-4"><p>THIRD PICK</p></div>
+        <div class="chamtitle pl-4 px-auto">
+          <h5 style="color: black">나만의 2인 최고 조합 추천</h5>
+        </div>
       </div>
       <b-row align-v="stretch" class="mb-0">
         <b-col cols="4" class="d-flex justify-content-center">
@@ -253,17 +266,17 @@
                   .replace('_1.jpg', '.png')
               "
             />
-            <b-badge pill variant="warning" class="pickname"
+            <b-badge pill variant="warning" class="pickname size1"
               >{{ cham_no3.chamname }} | {{ cham_no3.korname }}</b-badge
             >
-            <b-progress class="mt-3">
+            <b-progress class="mt-3" style="height:40px;">
               <b-progress-bar
                 :value="cham_no3_rate"
                 variant="info"
                 striped
                 :animated="animate"
               >
-                <span
+                <span style="font-size:20px;"
                   >승률: <strong>{{ cham_no3_rate }}%</strong></span
                 >
               </b-progress-bar>
@@ -274,7 +287,7 @@
           <b-row align-v="center" class="text-white">
             <b-col
               ><h3>
-                <span class="text-warning">{{ user.userNickname }}</span
+                <span class="text-red">{{ user.userNickname }}</span
                 >님이 가장 잘하는 챔피언은
                 <span class="text-warning">{{ cham_no3.korname }}</span>
               </h3></b-col
@@ -289,12 +302,14 @@
             >
           </b-row>
           <div class="simchams mt-5">
-            <div class="simtitle mb-5"><p>비슷한 성향을 가진 챔피언들</p></div>
+            <div class="simtitle mb-5">
+              <h5 style="color: black">비슷한 성향을 가진 챔피언들</h5>
+            </div>
             <b-row align-v="center">
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[6].imgsrc
                       .replace('@/assets/img', '')
@@ -305,7 +320,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[7].imgsrc
                       .replace('@/assets/img', '')
@@ -316,7 +331,7 @@
               <b-col>
                 <b-img
                   style="border-style: ridge; border: #fcd000 3px ridge"
-                  width="100%"
+                  width="150%"
                   :src="
                     sim_info[8].imgsrc
                       .replace('@/assets/img', '')
@@ -327,19 +342,19 @@
             </b-row>
             <b-row align-v="center">
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[6].chamname }} |
                   {{ sim_info[6].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[7].chamname }} |
                   {{ sim_info[7].korname }}</b-badge
                 >
               </b-col>
               <b-col>
-                <b-badge pill variant="warning" class="pickname"
+                <b-badge pill variant="warning" class="pickname size2"
                   >{{ sim_info[8].chamname }} |
                   {{ sim_info[8].korname }}</b-badge
                 >
@@ -500,16 +515,17 @@ export default {
 
 .chamtitle {
   position: relative;
-  width: 200px;
+  width: 400px;
   border-left: 30px solid transparent;
   border-right: 30px solid transparent;
   border-top: 30px solid #e3d19e;
   margin: 0px auto;
 }
 
-.chamtitle > p {
+.chamtitle > h5 {
   position: absolute;
   top: -30px;
+  left: 60px;
 }
 
 .simchams {
@@ -518,16 +534,25 @@ export default {
 
 .simtitle {
   position: relative;
-  width: 300px;
-  border-left: 20px solid transparent;
-  border-right: 20px solid transparent;
-  border-top: 20px solid #e3d19e;
+  width: 400px;
+  border-left: 30px solid transparent;
+  border-right: 30px solid transparent;
+  border-top: 30px solid #e3d19e;
   margin: 0px auto;
 }
 
-.simtitle > p {
+.simtitle > h5 {
   position: absolute;
-  top: -22px;
-  left: 30px;
+  top: -30px;
+  left: 60px;
+}
+.size1 {
+  font-size: 18px;
+}
+.size2 {
+  font-size: 15px;
+}
+.text-red {
+  color: red;
 }
 </style>
