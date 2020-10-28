14cfb39f90ca1a7a505b207bc8e69f019959ecd4
조완석 <wansuk771@gmail.com>
2020-10-28 16:18:39 +0900

    video-cnntest

--- djangobackend/demacia/ai/cnnmodel.py
+++ djangobackend/demacia/ai/cnnmodel.py
@@ -1,3 +1,4 @@
+from keras.models import load_model
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,13 +20,17 @@ y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=64, input_shape=(28,28,1), padding='same', activation='relu'),
-    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=64, padding='same', activation='relu'),
-    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=64, input_shape=(
+        28, 28, 1), padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=64,
+                           padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=128, padding='same', activation='relu'),
-    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=256, padding='valid', activation='relu'),
-    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=128,
+                           padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(kernel_size=(3, 3), filters=256,
+                           padding='valid', activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
 
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(units=512, activation='relu'),
@@ -35,12 +40,13 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(units=10, activation='softmax')
 ])
 
-model.compile(loss='categorical_crossentropy', optimizer=tf.optimizers.Adam(lr=0.001), metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.optimizers.Adam(lr=0.001), metrics=['accuracy'])
 model.summary()
 
-model.fit(x_train, y_train, batch_size=100, epochs=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, batch_size=100,
+          epochs=1, validation_data=(x_test, y_test))
 
 result = model.evaluate(x_test, y_test)
-# print("최종 예측 성공률(%): ", result[1]*100)
-from keras.models import load_model
+print("최종 예측 성공률(%): ", result[1]*100)
 model.save('mnist_mlp_model.h5')
