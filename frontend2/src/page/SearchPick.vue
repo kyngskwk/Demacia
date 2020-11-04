18fea29aa994f3f93f95bd4ad3a9b4e12647acf2
kyngskwk <kyngskwk@gmail.com>
2020-11-04 13:42:07 +0900

    FE usersinfo ing

--- frontend2/src/page/SearchPick.vue
+++ frontend2/src/page/SearchPick.vue
@@ -5,19 +5,37 @@
     <v-textarea 
         label="대기실 전적기록 검색하기"
         placeholder="대기실의 입장 안내 문구를 붙여넣기 해주세요!"
-        outlined>
-    </v-textarea>
-    <v-btn class="searchbtn blue-grey">검색하기</v-btn>
+        outlined
+        v-model="realtxt"
+    ></v-textarea>
+    <v-btn class="searchbtn blue-grey" @click="gosearch">검색하기</v-btn>
     <!-- 결과 안내창 -->
     <v-card class="mt-5">
-      <img src="../assets/img/pickinfo.png" alt="" class="pt-5 pb-3">
+      <img v-if="infoshow" src="../assets/img/pickinfo.png" alt="" class="pt-5 pb-3">
     </v-card>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'SearchPick'
+    name: 'SearchPick',
+    data() {
+      return {
+        realtxt:'',
+        infoshow:true,
+        inputtxt:'',
+      }
+    },
+    methods: {
+      gosearch() {
+        this.infoshow=false
+        this.inputtxt=this.realtxt.split('\n')
+        console.log(this.inputtxt)
+        axios.post(`localhost:8000/django/api/usersinfo/`)
+      }
+    }
 }
 </script>
 
