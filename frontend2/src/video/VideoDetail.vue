d9e5ed24522ec3496b020d66ce7f7b9e541fc416
조완석 <wansuk771@gmail.com>
2020-11-10 19:37:27 +0900

    TimeLine

--- frontend2/src/video/VideoDetail.vue
+++ frontend2/src/video/VideoDetail.vue
@@ -5,10 +5,28 @@
   <v-container>
     <v-row justify="space-around">
       <v-card width="800" >
-        <v-img
-          height="200px"
-          src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
-        >
+                <v-card-title class="black--text mt-2" >
+            <v-avatar size="56">
+              <img
+                alt="user"
+                src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+              >
+            </v-avatar>
+            <p class="ml-3 mt-2">
+              John Doe
+            </p>
+                        <p class="ml-10 mt-2">
+              EVENT TIMELINE
+            </p>
+          </v-card-title>
+       <video
+        style="width: 100%; padding: 0; max-width: 900px"
+        controls
+        autoplay="true"
+        name="media"
+        id="vid"
+      >
+        
        
           <v-app-bar
             flat
@@ -30,22 +48,13 @@
             </v-btn>
           </v-app-bar>
 
-          <v-card-title class="white--text mt-8">
-            <v-avatar size="56">
-              <img
-                alt="user"
-                src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
-              >
-            </v-avatar>
-            <p class="ml-3">
-              John Doe
-            </p>
-          </v-card-title>
-        </v-img>
+
+        <source src="./4659518008_01.mp4" type="video/mp4" />
+      </video>
 
         <v-card-text>
           <div class="font-weight-bold ml-8 mb-2">
-            Today
+            TIME LINE
           </div>
 
           <v-timeline
@@ -76,7 +85,17 @@
 
 <script>
 export default {
+    name: "VideoDetail1",
+    data: () => ({
+      messages: [
+        { time: '15:43', from:"샤코", color:"#3F51B5",  message: "대지 드래곤 KILL" },
+        { time: '15:45', from:"노틸러스", color:"#F44336",  message: "스웨인 KILL" },
+        { time: '15:54', from:"노틸러스", color:"#F44336",  message: "샤코 KILL" },
+        { time: '16:02', from:"아리", color:"#3F51B5",  message: "헤카림 KILL" },
+        { time: '16:05', from:"아리", color:"#3F51B5",  message: "노틸러스 KILL" },
 
+      ],
+    }),  
 }
 </script>
 
