fe50d12312405ff3f9e908002277e4e4e8a8b70b
조완석 <wansuk771@gmail.com>
2020-11-11 17:51:05 +0900

    add

--- frontend2/src/router/dist/index.dev.js
+++ frontend2/src/router/dist/index.dev.js
@@ -19,6 +19,12 @@ var _SearchPick = _interopRequireDefault(require("../page/SearchPick.vue"));
 
 var _VideoDetail = _interopRequireDefault(require("../video/VideoDetail.vue"));
 
+var _UserSearch = _interopRequireDefault(require("../page/UserSearch.vue"));
+
+var _boardMain = _interopRequireDefault(require("../page/board/boardMain"));
+
+var _boardWrite = _interopRequireDefault(require("../page/board/boardWrite"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // 메인 페이지
@@ -26,6 +32,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 // 사이드 네브바
 // 대기실 전적검색
 // 비디오 게시판
+// 유저 전적검색
+// 게시판
+// 게시판
 _vue["default"].use(_vueRouter["default"]);
 
 var _default = new _vueRouter["default"]({
@@ -34,6 +43,16 @@ var _default = new _vueRouter["default"]({
   {
     path: "/",
     component: _Main["default"]
+  }, // 게시판
+  {
+    path: "/board",
+    name: "board",
+    component: _boardMain["default"]
+  }, // 게시판 글쓰기
+  {
+    path: "/boardWrite",
+    name: "boardWrite",
+    component: _boardWrite["default"]
   }, // 로그인 페이지
   {
     path: "/login",
@@ -52,6 +71,10 @@ var _default = new _vueRouter["default"]({
     path: "/videodetail",
     name: "VideoDetail",
     component: _VideoDetail["default"]
+  }, {
+    path: "/usersearch",
+    name: "UserSearch",
+    component: _UserSearch["default"]
   }]
 });
 
