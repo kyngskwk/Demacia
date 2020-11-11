9e1daa9f8ad4988a9cb14c89ddfe599279d1ab1c
김준원 <abcd4436@naver.com>
2020-11-11 16:18:26 +0900

    FE LJ fix

--- frontend2/src/page/user/LoginJoin.vue
+++ frontend2/src/page/user/LoginJoin.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script>
-import login from "@/components/user/Login";
-import join from "@/components/user/Join";
+import Login from "@/components/user/Login";
+import Join from "@/components/user/Join";
 export default {
   components: {
     Login,
