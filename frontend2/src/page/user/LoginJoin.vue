8dbd40ea8e70bbe6e05800602787a756e6c56f0a
송다희 <jilhdd@naver.com>
2020-11-11 16:37:05 +0900

    [FE] 로그인/회원가입 색 변경

--- frontend2/src/page/user/LoginJoin.vue
+++ frontend2/src/page/user/LoginJoin.vue
@@ -1,9 +1,10 @@
 <template>
   <v-container fill-height>
     <v-card max-width="85%" class="mx-auto">
-      <v-tabs background-color="blue-grey" dark grow>
-        <v-tab> 로그인 </v-tab>
-        <v-tab> 회원가입 </v-tab>
+      <v-tabs dark grow hide-slider>
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab style="background:rgb(247,202,201)"> 로그인 </v-tab>
+        <v-tab style="background:rgb(146,168,209)"> 회원가입 </v-tab>
         <v-tab-item>
           <Login />
         </v-tab-item>
