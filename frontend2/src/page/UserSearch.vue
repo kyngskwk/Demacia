09641a9797a01ad701ef73dade6181e8e305582b
kyngskwk <kyngskwk@gmail.com>
2020-11-11 17:19:47 +0900

    FS-pick change url

--- frontend2/src/page/UserSearch.vue
+++ frontend2/src/page/UserSearch.vue
@@ -49,8 +49,8 @@ export default {
         gosearch() {
             if (this.nickname != '') {
                 this.isloading = true
-                axios.post(`http://127.0.0.1:8000/django/api/usersearch/`, {
-                // axios.post(`http://k3a502.p.ssafy.io/django/api/usersearch/`, {
+                // axios.post(`http://127.0.0.1:8000/django/api/usersearch/`, {
+                axios.post(`http://k3a502.p.ssafy.io/django/api/usersearch/`, {
                     nickname: this.nickname
                 }).then(response => {
                     this.result = response.data
